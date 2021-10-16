0f5fb0a7e242bd874f86b731952c7f8e2b31fc50
Akshita-5harma <87754597+Akshita-5harma@users.noreply.github.com>
2021-10-16 14:23:20 +0530

    Update constructor.cpp
    pretty printing, different operation using constructor

--- constructor.cpp
+++ constructor.cpp
@@ -4,26 +4,30 @@ using namespace std;
 class Complex{
     int a ,b;
     public:
-        Complex(void);//Constructor declaration
-        void PrintData(){
-            cout<<"Your number is: "<< a << " + " << b << " is "<<a+b<<endl;
+        Complex(void);                  //Constructor declaration
+        void PrintData()
+        {
+            cout<<"Your number is: "<< a << " * " << b << " is "<<a*b<<endl;
         }
 };
 
-Complex::Complex(void){ // Default Constructor 
-    a = 10;
-    b = 10;
+Complex::Complex(void)
+{                                     // Default Constructor 
+    a = 50;
+    b = 20;
 }
 
-void Properties(){
-// 1. It should be declared in the public section of the class
-// 2. They are automatically invoked whenever the object is created
-// 3. Do not have return types
-// 4. It can have default arguments
-// 5. We cannot refer to their addresses
+void Properties()
+{
+ // 1. It should be declared in the public section of the class
+ // 2. They are automatically invoked whenever the object is created
+ // 3. Do not have return types
+ // 4. It can have default arguments
+ // 5. We cannot refer to their addresses
 }
 
-int main(){
+int main()
+{
     Complex c;
     c.PrintData();
 return 0;
